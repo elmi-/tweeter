9be49da8d7a4816e35f3bac9a5b7b0a96229c7b6
elmi- <abdi.elmi@rocketmail.com>
2021-11-17 19:06:05 -0800

    implemented dynamic tweets functionality and AJAX post/get requests to save/retrieve data, in addtion to modifying css to handle profile images vs em elements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,86 @@
 /*
- * Client-side JS logic goes here
+* Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+*/
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const createTweetElement = function(tweet) {
+  const $tweet = $(
+    `
+    <article class="tweet">
+    <header>
+        <div>
+        <img src="${ tweet.user.avatars }" />
+          <h1>${ tweet.user.name }</h1>
+          </div>
+          <h2>${ tweet.user.handle }</h2>
+      </header>
+      <p>
+      ${ tweet.content.text }
+      </p>
+      <footer>
+        <em class="date">${ timeago.format(tweet.created_at) }</em>
+        <section class="icons">
+        <em class="fas fa-flag"></em>
+          <em class="fas fa-retweet"></em>
+          <em class="fas fa-heart"></em>
+        </section>
+        </footer>
+        </article>
+    `);
+    
+    return $tweet;
+};
+
+const renderTweets = function(tweets) {
+  // debugger;
+  for(const tweet of tweets) {
+    $('#tweet-container').append(createTweetElement(tweet));
+  }
+};
+
+$(document).ready(function() {
+  $(".new-tweet form").on("submit", function(e) {
+    e.preventDefault();
+    console.log("form submitted");
+
+    var form = $(this);
+    
+    $.post("/tweets", form.serialize(), (data) => {
+      console.log("data", data)
+    });
+  });
+  
+  const loadtweets = () => {
+    $.get("/tweets", (data) => {
+      renderTweets(data);
+    });
+  };
+
+  loadtweets(); 
+});
